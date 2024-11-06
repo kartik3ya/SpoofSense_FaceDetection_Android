7185252f5d712bb63d1a0e5d0b885e352a8fe24c
DharmeshKanjariya <63076837+DharmeshKanjariya@users.noreply.github.com>
2024-11-06 23:38:54 +0530

    completed m1

--- app/src/main/java/com/spoofsense/facedetection/HomeActivity.java
+++ app/src/main/java/com/spoofsense/facedetection/HomeActivity.java
@@ -28,20 +28,16 @@ public class HomeActivity extends AppCompatActivity {
         tv_app_title = findViewById(R.id.tv_app_title);
         tv_app_subtitle = findViewById(R.id.tv_app_subtitle);
 
-        try {
-            versionName = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            versionName = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
         tv_app_title.setText(Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY), TextView.BufferType.SPANNABLE);
-        tv_app_subtitle.setText("face v" + versionName);
+        tv_app_subtitle.setText("face v2.1.0");
 
         btn_check_liveness.setOnClickListener(this::onCheckLivenessButtonClick);
-
-
-
-
     }
 
     // Button click method with Intent
